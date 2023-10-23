b8b6c26a2262522217f94ee6055350f12ecfa61d
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-23 05:16:22 -0400

    PersonCardView is fixed

--- CardView/PersonCardView.swift
+++ CardView/PersonCardView.swift
@@ -11,43 +11,44 @@ struct PersonCardView: View {
     let person: Person
     
     var body: some View {
-        VStack{
-            ZStack {
-            Image(person.headerImage)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(height: 80)
-                .clipped()
+        VStack {
             
-            Image(person.profileImage)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 80)
-                .clipShape(Circle())
-                .offset(y: -40)
-                .padding(.horizontal)
-        }
-        .overlay(alignment: .topTrailing) {
-            Button {
-                // ?
-            } label: {
-                Image(systemName: "xmark.circle.fill")
-                    .foregroundColor(.white)
-                    .padding(6)
+            ZStack {
+                Image(person.headerImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 80)
+                    .clipped()
+                
+                Image(person.profileImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 80)
+                    .clipShape(Circle())
+                    .offset(y: 40)
+                    .padding(.horizontal)
             }
-        }
-        
-            VStack(spacing: -31.0) {
-                Button {
-                    // ?
-                } label: {
+            .overlay(alignment: .topTrailing) {
+                Button(action: {
+                    // Ação do botão
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.white)
+                        .padding(6)
+                }
+                
+            }
+            VStack(spacing: 0.0) {
+                Button(action: {
+                    // Ação do botão
+                }) {
                     Text("Follow")
-                    padding(.vertical, 4)
-                    padding(.horizontal)
+                        .padding(.vertical, 4)
+                        .padding(.horizontal)
                         .overlay {
                             Capsule()
                                 .stroke(lineWidth: 2)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+//                            .background(Capsule().stroke(Color.yellow, lineWidth: 2))
                         }
                 }
                 .frame(maxWidth: .infinity, alignment: .trailing)
@@ -67,13 +68,12 @@ struct PersonCardView: View {
                 Text(person.jobTitle)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding([.leading, .bottom])
-                
             }
+//            .padding(.horizontal)
         }
-        .background(Color(.tertiarySystemFill))
-        .cornerRadius(12)
     }
 }
+
 
 struct PersonCardView_Previews: PreviewProvider {
     static var previews: some View {
