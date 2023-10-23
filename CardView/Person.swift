3e73f6fd500d48a2a6229f8a6258509c74556680
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-22 22:23:06 -0400

    Person and PersonCardView implemented

---
+++ CardView/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  CardView
+//
+//  Created by Marcelo Amaral Alves on 2023-10-22.
+//
+
+import Foundation
---
+++ CardView/PersonCardView.swift
@@ -0,0 +1,18 @@
+//
+//  PersonCardView.swift
+//  CardView
+//
+//  Created by Marcelo Amaral Alves on 2023-10-22.
+//
+
+import SwiftUI
+
+struct PersonCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonCardView()
+}
