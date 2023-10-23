95c5ba419a21ad65d380f6294b9c2ed1f565bdc3
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-22 22:06:44 -0400

    Initial Commit

---
+++ CardView/CardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewApp.swift
+//  CardView
+//
+//  Created by Marcelo Amaral Alves on 2023-10-22.
+//
+
+import SwiftUI
+
+@main
+struct CardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
