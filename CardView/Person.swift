754acd61f1bc2c791d6127847e9471a645de5520
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-22 22:53:16 -0400

    fixing person

--- CardView/Person.swift
+++ CardView/Person.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Person {
+    let headerImage: String
+    let profileImage: String
+    let userName: String
+    let followerCount: Int
+    let jobTitle: String
+}
+
+let person1 = Person(headerImage: "headerImage1", profileImage: "profileImage1", userName: "Lewis Hamilton", followerCount: 1000, jobTitle: "Taxi Driver")
+
+let person2 = Person(headerImage: "headerImage2", profileImage: "profileImage2", userName: "Fernando Alonso", followerCount: 1000, jobTitle: "Cyclist")
+
